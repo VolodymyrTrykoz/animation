7520bd0b17c0518c73cd4cbc7fb9d7d9e5dc28e8
VolodymyrTrykoz <mr.mastachok@gmail.com>
2017-07-25 17:14:01 +0300

    25.07.2017

--- js/js.js
+++ js/js.js
@@ -4,6 +4,9 @@ $('.slider').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   dots: true,
-  fade: true,
+  fade: true,//without this property slider can't open full screen animated img. Miracle
+ // autoplay: true,
+  autoplaySpeed: 10000,
+  draggable: false,
  
 });
