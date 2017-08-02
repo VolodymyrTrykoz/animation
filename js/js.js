ee737fed6a1445b9534ea9c4761c10e78cab5ad2
VolodymyrTrykoz <mr.mastachok@gmail.com>
2017-08-02 16:02:54 +0300

    '02/08/17'

--- js/js.js
+++ js/js.js
@@ -27,8 +27,33 @@ jQuery(function($){
 	$('.eqheigth').height(max_col_height); // устанавливаем высоту каждой колонки равной значению максимальной высоты
 });
 //---------------------------------------------
+    //3 divs small scroll animation
 	$(window).scroll(function(){
     var bo = $("body").scrollTop();
-    if ( bo > 200 ) $("#world > div").css({'-webkit-transform': 'translate(0)'}, {'-webkit-transition-duration':'1.2s'});
+    if ( bo > 50 ) $("#world > div").css({'opacity':'1', 
+    									   '-webkit-transform': 'translate(0)'});
      });
-
+//---------------------------------------------
+	$(window).scroll(function(){
+	var bo = $('body').scrollTop();
+	//$('#reasons_wrapper').text(bo);
+	if ( bo > 1200 ) $('#reasons_wrapper').css({'top':'0',
+												'opacity':'1'});
+	});
+//---------------------------------------------	
+jQuery(function($){
+	var max_col_height = 0; // максимальная высота, первоначально 0
+	$('.equal_size_small').each(function(){ // цикл "для каждой из колонок"
+		if ($(this).height() > max_col_height) { // если высота колонки больше значения максимальной высоты,
+			max_col_height = $(this).height(); // то она сама становится новой максимальной высотой
+		}
+	});
+	$('.equal_size_small').height(max_col_height); // устанавливаем высоту каждой колонки равной значению максимальной высоты
+});
+//---------------------------------------------
+	$(window).scroll(function(){
+	var bo = $('body').scrollTop();
+	//$('#specialism_wrapper').text(bo);
+	if ( bo > 2300 ) $('#specialism_wrapper').css({'right':'0',
+												'opacity':'1'});
+	});
